1f5ef48e8f56bd01c3900f1e224722b3fd112af0
WinsomeQuill <Artemqa.belyakov07@gmail.com>
2022-01-26 19:20:19 +0700

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@
 // Author: WinsomeQuill (https://github.com/WinsomeQuill)
 
 use std::{collections::HashMap, thread, time};
-use conversion_tools_api::api::{get_tasks, upload_file, create_task, get_task, download_file};
-use serde_json::{Value, from_str};
+use conversion_tools_api::api::{upload_file, create_task, get_task, download_file};
+use serde_json::Value;
 
 const TOKEN: &str ="YOUR TOKEN";
 const URL: &str = "https://api.conversiontools.io/v1/"; //API URL
@@ -13,20 +13,19 @@ const URL: &str = "https://api.conversiontools.io/v1/"; //API URL
 fn main() {
     file_convert_example(&"king_cat.jpg");
     site_convert_example(&"https://quartzland.ru/");
-    //get_all_task_example();
 }
 
 fn file_convert_example(path: &str) {
-    let mut args: HashMap<&str, &str> = HashMap::new();
-    args.insert("orientation", "Portrait");
+    let mut args: HashMap<&str, &str> = HashMap::new(); //create HashMap for arguments
+    args.insert("orientation", "Portrait"); //argument for convertor
 
-    let upload_result: Value = from_str(&upload_file(URL, TOKEN, &path)).unwrap();
-    let create_task_result: Value = from_str(&create_task(URL, TOKEN, &"convert.jpg_to_pdf", &upload_result["file_id"].as_str().unwrap(), &args)).unwrap();
-    let mut get_task_result: Value = from_str(&get_task(URL, TOKEN, &create_task_result["task_id"].as_str().unwrap())).unwrap();
+    let upload_result: Value = upload_file(URL, TOKEN, &path);
+    let create_task_result: Value = create_task(URL, TOKEN, &"convert.jpg_to_pdf", &upload_result["file_id"].as_str().unwrap(), &args);
+    let mut get_task_result: Value = get_task(URL, TOKEN, &create_task_result["task_id"].as_str().unwrap());
 
     while get_task_result["status"] != "SUCCESS" {
         thread::sleep(time::Duration::from_millis(1500));
-        get_task_result = from_str(&get_task(URL, TOKEN, &create_task_result["task_id"].as_str().unwrap())).unwrap();
+        get_task_result = get_task(URL, TOKEN, &create_task_result["task_id"].as_str().unwrap());
         println!("Wait...");
     }
 
@@ -36,24 +35,20 @@ fn file_convert_example(path: &str) {
 }
 
 fn site_convert_example(site_url: &str) {
-    let mut hash: HashMap<&str, &str> = HashMap::new();
-    hash.insert("url", &site_url);
-    hash.insert("images", "yes");
-    hash.insert("javascript", "yes");
+    let mut hash: HashMap<&str, &str> = HashMap::new(); //create HashMap for arguments
+    hash.insert("url", &site_url); //argument for convertor
+    hash.insert("images", "yes"); //argument for convertor
+    hash.insert("javascript", "yes"); //argument for convertor
 
-    let create_task_result: Value = from_str(&create_task(URL, TOKEN, &"convert.website_to_png", "", &hash)).unwrap();
-    let mut get_task_result: Value = from_str(&get_task(URL, TOKEN, &create_task_result["task_id"].as_str().unwrap())).unwrap();
+    let create_task_result: Value = create_task(URL, TOKEN, &"convert.website_to_png", "", &hash);
+    let mut get_task_result: Value = get_task(URL, TOKEN, &create_task_result["task_id"].as_str().unwrap());
     while get_task_result["status"] != "SUCCESS" {
         thread::sleep(time::Duration::from_millis(1500));
-        get_task_result = from_str(&get_task(URL, TOKEN, &create_task_result["task_id"].as_str().unwrap())).unwrap();
+        get_task_result = get_task(URL, TOKEN, &create_task_result["task_id"].as_str().unwrap());
         println!("Wait...");
     }
 
     println!("Downloading...");
     download_file(URL, TOKEN, &get_task_result["file_id"].as_str().unwrap(), "conv.png");
     println!("Okay");
-}
-
-fn get_all_task_example() {
-   println!("{}", get_tasks(URL, TOKEN))
 }
