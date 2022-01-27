33710dbb09ebe580fb5041b1e608f37375b44e13
WinsomeQuill <Artemqa.belyakov07@gmail.com>
2022-01-27 17:08:54 +0700

    Updated for version 0.1.3

--- src/main.rs
+++ src/main.rs
@@ -20,19 +20,42 @@ fn file_convert_example(object: &Api, path: &str) {
     let mut args: HashMap<&str, &str> = HashMap::new(); //create HashMap for arguments
     args.insert("orientation", "Portrait"); //argument for convertor
 
-    let upload_result: Value = object.upload_file(&path);
-    let create_task_result: Value = object.create_task(&"convert.jpg_to_pdf", &upload_result["file_id"].as_str().unwrap(), &args);
-    let mut get_task_result: Value = object.get_task(&create_task_result["task_id"].as_str().unwrap());
+    let upload_result: Value = object.upload_file(&path).expect("Error when upload file!");
 
-    while get_task_result["status"] != "SUCCESS" {
-        thread::sleep(time::Duration::from_millis(1500));
-        get_task_result = object.get_task(&create_task_result["task_id"].as_str().unwrap());
-        println!("Wait...");
+    if !upload_result["error"].as_str().is_none() {
+        panic!("{}", upload_result["error"]);
     }
 
-    println!("Downloading...");
-    object.download_file(&get_task_result["file_id"].as_str().unwrap(), "result.pdf");
-    println!("Okay");
+    let create_task_result: Value = object.create_task(&"convert.jpg_to_pdf", &upload_result["file_id"].as_str().unwrap(), &args).expect("Error when create task!");
+
+    if !create_task_result["error"].as_str().is_none() {
+        panic!("{}", create_task_result["error"]);
+    }
+
+    let mut get_task_result: Value = object.get_task(&create_task_result["task_id"].as_str().unwrap()).expect("Error when get task!");
+
+    if !get_task_result["error"].as_str().is_none() {
+        panic!("{}", get_task_result["error"]);
+    }
+
+    loop {
+        get_task_result = object.get_task(&create_task_result["task_id"].as_str().unwrap()).expect("Error when get task!");
+        if get_task_result["status"].as_str().unwrap() == "SUCCESS" {
+            break;
+        } else if get_task_result["status"].as_str().unwrap() == "ERROR" {
+            println!("[File Convert] Error => {}", get_task_result["error"]);
+            return;
+        } else {
+            println!("[File Convert] Wait...");
+            thread::sleep(time::Duration::from_millis(1500));
+        }
+    }
+
+    println!("[File Convert] Downloading...");
+    match object.download_file(get_task_result["file_id"].as_str().unwrap(), "result.pdf") {
+        Ok(_) => println!("[File Convert] Okay"),
+        Err(e) => panic!("[File Convert] {}", e),
+    };
 }
 
 fn site_convert_example(object: &Api, site_url: &str) {
@@ -41,15 +64,34 @@ fn site_convert_example(object: &Api, site_url: &str) {
     hash.insert("images", "yes"); //argument for convertor
     hash.insert("javascript", "yes"); //argument for convertor
 
-    let create_task_result: Value = object.create_task(&"convert.website_to_png", "", &hash);
-    let mut get_task_result: Value = object.get_task(&create_task_result["task_id"].as_str().unwrap());
-    while get_task_result["status"] != "SUCCESS" {
-        thread::sleep(time::Duration::from_millis(1500));
-        get_task_result = object.get_task(&create_task_result["task_id"].as_str().unwrap());
-        println!("Wait...");
+    let create_task_result: Value = object.create_task(&"convert.website_to_png", "", &hash).expect("Error when create task!");
+
+    if !create_task_result["error"].as_str().is_none() {
+        panic!("{}", create_task_result["error"]);
     }
 
-    println!("Downloading...");
-    object.download_file(&get_task_result["file_id"].as_str().unwrap(), "conv.png");
-    println!("Okay");
+    let mut get_task_result: Value = object.get_task(&create_task_result["task_id"].as_str().unwrap()).expect("Error when get task!");
+
+    if !get_task_result["error"].as_str().is_none() {
+        panic!("{}", get_task_result["error"]);
+    }
+
+    loop {
+        get_task_result = object.get_task(create_task_result["task_id"].as_str().unwrap()).expect("Error when get task!");
+        if get_task_result["status"].as_str().unwrap() == "SUCCESS" {
+            break;
+        } else if get_task_result["status"].as_str().unwrap() == "ERROR" {
+            println!("[Site Convert] Error => {}", get_task_result["error"]);
+            return;
+        } else {
+            println!("[Site Convert] Wait...");
+            thread::sleep(time::Duration::from_millis(1500));
+        }
+    }
+
+    println!("[Site Convert] Downloading...");
+    match object.download_file(get_task_result["file_id"].as_str().unwrap(), "conv.png") {
+        Ok(_) => println!("[Site Convert] Okay"),
+        Err(e) => panic!("[Site Convert] {}", e),
+    };
 }
